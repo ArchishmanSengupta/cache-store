27316131fb0c7cd25f5a3a64011ef642254e2fe0
Archishman Sengupta <archishman1808@gmail.com>
2024-06-17 21:01:31 +0530

    feat: go routine to periodically clean up expired items

--- cache.go
+++ cache.go
@@ -1,3 +1,10 @@
+/*
+	The purpose of this code is to implement a thread-safe in-memory cache with support
+	for optional expiration times for stored items.
+
+	This cache can store any type of data and automatically remove expired items at specified intervals.
+*/
+
 package cache
 
 import (
@@ -9,7 +16,7 @@ import (
 
 // CacheStore represents a cache store that holds arbitrary data with expiration time.
 type CacheStore struct {
-	items            sync.Map
+	items            sync.Map // for thread-safe
 	ctx              context.Context
 	cancel           context.CancelFunc
 	cleaningInterval time.Duration
@@ -23,19 +30,52 @@ type CachedItem struct {
 
 // NewCacheStore creates a new cache store asynchronously cleans expired entries after the given time passes
 func NewCacheStore(cleaningInterval time.Duration) *CacheStore {
+	// Validates that the cleaningInterval is positive; logs a fatal error and stops the program if not.
 	if cleaningInterval <= 0 {
 		log.Fatal("cleaning interval must be positive")
 	}
 
+	// Creates a cancellable context using context.WithCancel.
 	ctx, cancel := context.WithCancel(context.Background())
 
+	// Initializes a CacheStore with the context, cancel function, and cleaning interval.
 	cacheStore := &CacheStore{
 		ctx:              ctx,
 		cancel:           cancel,
 		cleaningInterval: cleaningInterval,
 	}
 
-	//go cacheStore.cleanupExpiredItems();
+	// Starts a goroutine that runs the cleanupExpiredItems method to periodically remove expired items.
+	// go cacheStore.cleanupExpiredItems();
 
+	// Returns the initialized CacheStore.
 	return cacheStore
+}
+
+// Go routine to periodically clean up expired items
+func (cacheStore *CacheStore) cleanupExpiredItems() {
+	// creates a time.Ticker that ticks at intervals specified by 'cleaning Intervals'
+	ticker := time.NewTicker(cacheStore.cleaningInterval)
+
+	// Stop the ticker after the function exits
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			// on each tick it gets the current time in nanoseconds
+			currentTime := time.Now().UnixNano()
+
+			// iterates over all the items in the cache, checking each item's expiration time
+			cacheStore.items.Range(func(key, value interface{}) bool {
+				item := value.(CachedItem)
+
+				// if an item is expired it deletes the item from the cache
+				if item.expires > 0 && currentTime > item.expires {
+					cacheStore.items.Delete(key)
+				}
+				return true
+			})
+		}
+	}
 }
